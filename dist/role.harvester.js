221f7409a925634653f4576dd7317871bd97cec6
Thomas Müller <thomas.mueller@tmit.eu>
2019-12-25 11:18:59 +0100

    use floor

--- dist/role.harvester.js
+++ dist/role.harvester.js
@@ -4,7 +4,7 @@ var roleHarvester = {
     run: function(creep) {
 	    if(creep.store.getFreeCapacity() > 0) {
             var sources = creep.room.find(FIND_SOURCES);
-            const index = Math.ceil(Math.random() * sources.length)
+            const index = Math.floor(Math.random() * sources.length)
             if(creep.harvest(sources[index]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[index], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
