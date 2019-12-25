fd19408fe3c73ad581b562879fe2c421219100bc
Thomas Müller <thomas.mueller@tmit.eu>
2019-12-25 01:17:19 +0100

    Build road to controller

--- dist/roadBuilder.js
+++ dist/roadBuilder.js
@@ -1,6 +1,7 @@
 
 
 module.exports.run = function(spawnName) {
+    // build roads to sources
     var sources = Game.spawns[spawnName].room.find(FIND_SOURCES);
     for (var j = 0; j < sources.length; j++)
     {
@@ -9,5 +10,12 @@ module.exports.run = function(spawnName) {
         {
             Game.spawns[spawnName].room.createConstructionSite(chemin[i].x,chemin[i].y, STRUCTURE_ROAD);
         }
+    }
+
+    // build roads to the controller
+    var chemin = Game.spawns[spawnName].pos.findPathTo(Room.controller.pos);
+    for (var i = 0; i < chemin.length; i++) 
+    {
+        Game.spawns[spawnName].room.createConstructionSite(chemin[i].x,chemin[i].y, STRUCTURE_ROAD);
     }
 }
