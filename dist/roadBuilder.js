f21fa5d4bda71070f0e2133d0074c21f5aaf12a0
Thomas Müller <thomas.mueller@tmit.eu>
2019-12-27 13:18:53 +0100

    Roadbuilding: only 10 sites ...

--- dist/roadBuilder.js
+++ dist/roadBuilder.js
@@ -7,6 +7,12 @@ function buildRoad(spawnName, chemin) {
 }
 
 module.exports.run = function(spawnName) {
+    // only have 10 road sites max
+    const sites = Game.spawns[spawnName].room.find(FIND_MY_CONSTRUCTION_SITES);
+    if (sites.length > 10) {
+        console.log(sites.length + ' sites under construction ....')
+        return
+    }
     var sources = Game.spawns[spawnName].room.find(FIND_SOURCES);
     for (var j = 0; j < sources.length; j++)
     {
