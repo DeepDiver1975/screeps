760058805e554b33c1a4793c57c20e7ea6269b90
Thomas Müller <thomas.mueller@tmit.eu>
2019-12-27 12:45:05 +0100

    Build roads from source to controller

--- dist/roadBuilder.js
+++ dist/roadBuilder.js
@@ -1,21 +1,20 @@
 
-
-module.exports.run = function(spawnName) {
-    // build roads to sources
-    var sources = Game.spawns[spawnName].room.find(FIND_SOURCES);
-    for (var j = 0; j < sources.length; j++)
-    {
-        var chemin = Game.spawns[spawnName].pos.findPathTo(sources[j].pos);
-        for (var i = 0; i < chemin.length; i++) 
-        {
-            Game.spawns[spawnName].room.createConstructionSite(chemin[i].x,chemin[i].y, STRUCTURE_ROAD);
-        }
-    }
-
-    // build roads to the controller
-    var chemin = Game.spawns[spawnName].pos.findPathTo(Game.spawns[spawnName].room.controller.pos);
+function buildRoad(spawnName, chemin) {
     for (var i = 0; i < chemin.length; i++) 
     {
         Game.spawns[spawnName].room.createConstructionSite(chemin[i].x,chemin[i].y, STRUCTURE_ROAD);
+    }
+}
+
+module.exports.run = function(spawnName) {
+    var sources = Game.spawns[spawnName].room.find(FIND_SOURCES);
+    for (var j = 0; j < sources.length; j++)
+    {
+        // spawn <-> sources
+        var chemin = Game.spawns[spawnName].pos.findPathTo(sources[j].pos);
+        buildRoad(spawnName, chemin)
+        // controller <-> sources
+        var chemin = Game.spawns[spawnName].room.controller.pos.findPathTo(sources[j].pos);
+        buildRoad(spawnName, chemin)
     }
 }
