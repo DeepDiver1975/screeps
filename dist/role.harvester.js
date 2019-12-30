a74dea02e3329da253cb6fb861391b7919cfac9e
Thomas Müller <thomas.mueller@tmit.eu>
2019-12-30 15:20:31 +0100

    Store energy in closest storage

--- dist/role.harvester.js
+++ dist/role.harvester.js
@@ -12,7 +12,7 @@ var roleHarvester = {
             }
         }
         else {
-            var targets = creep.room.find(FIND_STRUCTURES, {
+            var closestStorage = creep.pos.findClosestByRange(FIND_STRUCTURES, {
                     filter: (structure) => {
                         return (structure.structureType == STRUCTURE_EXTENSION ||
                                 structure.structureType == STRUCTURE_SPAWN ||
@@ -20,9 +20,9 @@ var roleHarvester = {
                                 structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
                     }
             });
-            if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+            if(closestStorage) {
+                if(creep.transfer(closestStorage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(closestStorage, {visualizePathStyle: {stroke: '#ffffff'}});
                 }
             }
         }
