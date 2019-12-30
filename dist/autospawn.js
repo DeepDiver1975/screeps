49a4efc15804930dc350f9ae6eb15a1b79b8a902
Thomas Müller <thomas.mueller@tmit.eu>
2019-12-31 00:34:39 +0100

    Back to powerful creeps

--- dist/autospawn.js
+++ dist/autospawn.js
@@ -11,7 +11,7 @@
     if(harvesters.length <= limit) {
         var newName = role + '-' + Game.time;
         console.log('Spawning new ' + role + ': ' + newName);
-        Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName,
+        Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE,WORK,CARRY,MOVE], newName,
             {memory: {role: role}});
     }
  }
