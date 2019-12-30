8af900368f2ebd356faaa2a1ae7b06363333fbfb
Thomas Müller <thomas.mueller@tmit.eu>
2019-12-30 22:29:30 +0100

    Explicit repairer

--- dist/autospawn.js
+++ dist/autospawn.js
@@ -30,9 +30,11 @@ module.exports.popControl = function() {
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
     var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
-    console.log('Population: ' + harvesters.length + ' H - ' + builders.length + ' B - ' + upgraders.length + ' U');
+    var repairers = _.filter(Game.creeps, (creep) => creep.memory.role == 'repairer');
+    console.log('Population: ' + harvesters.length + ' H - ' + builders.length + ' B - ' + upgraders.length + ' U - ' + repairers.length + ' R');
 
-    spawn(harvesters, 'harvester', pop / 3)
-    spawn(builders, 'builder', pop / 3)
-    spawn(upgraders, 'upgrader', pop / 3)
+    spawn(harvesters, 'harvester', 5)
+    spawn(builders, 'builder', 4)
+    spawn(upgraders, 'upgrader', 4)
+    spawn(repairers, 'repairer', 2)
 }
--- dist/role.repairer.js
+++ dist/role.repairer.js
@@ -11,15 +11,19 @@ var roleBuilder = {
 	    }
 	    if(!creep.memory.building && creep.store.getFreeCapacity() == 0) {
 	        creep.memory.building = true;
-	        creep.say('🚧 build');
+	        creep.say('🚧 repair');
 	    }
 
 	    if(creep.memory.building) {
-			const closestContructionSite = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
-            if(closestContructionSite) {
-                if(creep.build(closestContructionSite) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(closestContructionSite, {visualizePathStyle: {stroke: '#ffffff'}});
-                }
+			var percentage = 0.01;
+			var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+				filter: (s) => (s.structureType === STRUCTURE_WALL || s.structureType === STRUCTURE_ROAD) &&
+				s.hits / s.hitsMax < percentage
+			});
+			if (!target == 0) {
+				if (creep.repair(target) == ERR_NOT_IN_RANGE) {
+					creep.moveTo(target);
+				}
 			}
 	    }
 	    else {
