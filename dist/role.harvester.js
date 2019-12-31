e8a2c83ae580484a9ef4cbd7f943dd587ac97c7b
Thomas Müller <thomas.mueller@tmit.eu>
2019-12-31 12:40:34 +0100

    Store energy in storage as well

--- dist/role.harvester.js
+++ dist/role.harvester.js
@@ -15,9 +15,10 @@ var roleHarvester = {
             var closestStorage = creep.pos.findClosestByRange(FIND_STRUCTURES, {
                     filter: (structure) => {
                         return (structure.structureType == STRUCTURE_EXTENSION ||
-                                structure.structureType == STRUCTURE_SPAWN ||
-                                structure.structureType == STRUCTURE_TOWER) && 
-                                structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                            structure.structureType == STRUCTURE_CONTAINER ||
+                            structure.structureType == STRUCTURE_SPAWN ||
+                            structure.structureType == STRUCTURE_TOWER) && 
+                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
                     }
             });
             if(closestStorage) {
