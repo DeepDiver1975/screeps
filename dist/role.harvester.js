e06eb9d4287a1a0e3c573e799d83be6ef552817e
Thomas Müller <thomas.mueller@tmit.eu>
2019-12-31 12:49:25 +0100

    Fix capacity check for storages as well

--- dist/role.harvester.js
+++ dist/role.harvester.js
@@ -18,7 +18,7 @@ var roleHarvester = {
                             structure.structureType == STRUCTURE_CONTAINER ||
                             structure.structureType == STRUCTURE_SPAWN ||
                             structure.structureType == STRUCTURE_TOWER) && 
-                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                            (_.sum(structure.store) < structure.storeCapacity);
                     }
             });
             if(closestStorage) {
